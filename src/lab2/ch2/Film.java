df646f283dd2fb8d584dbfb4c648cb7f023e69fa
Banu Ionela <ionelamihaela003@gmail.com>
2021-11-08 20:50:56 +0200

    lab2

---
+++ src/lab2/ch2/Film.java
@@ -0,0 +1,22 @@
+package lab2.ch2;
+
+public class Film {
+
+    private int anAparitie;
+    private String nume;
+    private Actor [] actori;
+
+    public Film(int anAparitie, String nume, Actor[] actori) {
+        this.anAparitie = anAparitie;
+        this.nume = nume;
+        this.actori = actori;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public Actor[] getActori() {
+        return actori;
+    }
+}
